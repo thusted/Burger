8085c49ea16621d9d2f3b62dc97cb23df73e3501
Tiana Husted <tiana.husted@gmail.com>
2020-09-07 19:18:19 -0700

    Creatin db folder to hold schema.sql and seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chevre swiss", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheddar bleu", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pepper jack", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lettuce wrapped", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The clogger", true);
