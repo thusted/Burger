2889a5bcd5800381fe38f62ef8f88f52bfc239bf
Tiana Husted <tiana.husted@gmail.com>
2020-09-10 15:34:55 -0700

    Updating code to make devour button functional

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "love_bob1",
+  password: "", //Your password goes here
   database: "burgers_DB"
 });
 
